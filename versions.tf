9fa404dc95a80decb1d4f6791b9cbb9b1409982a
Steven Pall <mail@stevenpall.ca>
2022-10-04 16:29:59 -0700

    Bump required_providers

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~>3.25"
+      source  = "hashicorp/aws"
+      version = "~> 4.23.0"
     }
     google = {
       source = "hashicorp/google"
-      version = "~> 3.3"
+      version = "~> 4.25.0"
     }
   }
   required_version = ">= 0.13"
